080767c705a42752b839b83e72b71295bfdc149a
gfragiadakis <gabi.fragiadakis@gmail.com>
2015-10-22 13:20:11 -0700

    Added annotations to data

--- analyses/data-structures-external-data.R
+++ analyses/data-structures-external-data.R
@@ -1,52 +1,57 @@
 library(dplyr)
 library(reshape2)
+library(magrittr)
 
 # Read in data from External query
 
 human_table <- read.table("~/Documents/FDAlibrary/saved_data_structures/signaling-for-gabi.tsv", sep="\t")
-colnames(human_table) <- c("Condition", "ID_unformatted", "Population", "Reagent", "Raw_median")
+colnames(human_table) <- c("Condition", "Donor", "Population", "Reagent", "Raw_median")
 human_df <- data.frame(human_table)
+human_df$Raw_median <- as.numeric(as.character(human_df$Raw_median))
 
 # Convert names and store each
-population_key <- cbind(levels(human_df$Population), convert_names(levels(human_df$Population)))
-colnames(population_key) <- c("Original", "Converted")
+population_key <- data.frame(Original = levels(human_df$Population),
+                             Converted =convert_names(levels(human_df$Population)))
 
-reagent_key <- cbind(levels(human_df$Reagent), convert_names(levels(human_df$Reagent)))
-colnames(reagent_key) <- c("Original", "Converted")
+reagent_key <- data.frame(Original = levels(human_df$Reagent),
+                          Converted =convert_names(levels(human_df$Reagent)))
 
 # Make features (Population x Reagent)
 human_df$Population <- as.factor(convert_names(human_df$Population))
 human_df$Reagent <- as.factor(convert_names(human_df$Reagent))
-# Filter out those dumb pops
 
-Feature <- paste(human_df$Population, human_df$Reagent, sep = "_")
-human_df <- cbind(human_df, Feature)
-feature_matrix <- dplyr::select(human_df, Population, Reagent, Feature)
-human_df <- dplyr::select(human_df, Condition, ID_unformatted, Feature, Raw_median)
+# Filter out extra populations
+
+new_df <- human_df %>%
+  dplyr::filter(Population != "CD4..CD8.") %>%
+  dplyr::filter(Population != "HLA.DR..CD11c.") %>%
+  dplyr::filter(Population != "singlets_basophils.") %>%
+  dplyr::mutate(Feature = paste(Population, Reagent, sep = "_"))
+
+new_df$Feature <- as.factor(new_df$Feature)
+feature_matrix <- dplyr::select(new_df, Population, Reagent, Feature)
+
+new_df <- dplyr::select(new_df, Donor, Condition, Feature, Raw_median)
+
 
 # Change to wide format
-wide_human_df <- dcast(human_df, Condition + ID_unformatted ~ Feature)
+
+# data clean up (Messups in the data that will be fixed with new sheets)
+new_df <- unique(new_df)
+
+wide_df <- dcast(new_df, Donor + Condition ~ Feature, value.var = "Raw_median")
 
 
+# Get the annotations from the server
 
-
-
-
-
-
-
-## Random garbage may use later
-
-access_key <- authenticate("gfragiadakis", "password", baseURL =  "http://52.27.144.218/api/v1")
+access_key <- authenticate("gfragiadakis", "password", baseURL =  "http://54.191.150.29/api/v1")
 
 experiments <- get_experiments(access_key)
 
 experimentID <- experiments[experiments$name == "All Species", '_id']
-
-
-# get_experiment(experimentID, access_key)
-
 FCS_files <- get_FCS_files(experimentID, access_key)
-FCS_files <- FCS_files[1:2624,]
+FCS_files <- FCS_files[1:2624,] # should fix annotations function so don't have to do it
 annotations <- get_annotations(FCS_files)
-Humans <- dplyr::filter(annotations,Species == "Human")
+human_annotations <- dplyr::filter(annotations,Species == "Human")
+
+annotated_df <- merge(human_annotations, wide_df)
