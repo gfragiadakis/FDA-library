70b4e8d4cb5f7bb6cbf16c74c3acc57797113a13
gfragiadakis <gabi.fragiadakis@gmail.com>
2015-11-18 14:34:38 -0800

    Finalize analysis pipeline external data

--- analyses/data-structures-external-data.R
+++ analyses/data-structures-external-data.R
@@ -1,17 +1,19 @@
 library(FDAlibrary)
 library(dplyr)
 library(reshape2)
-library(magrittr)
 
 # Notes:
 # 7605 donated twice: R20W3_7605 and R19W3_7605.
 # 7962 donated twice: R20W11_7962 and R25W5_7962
 # R23 has incorrect metadata
 
+# set data for structure output
+dataDirectory <- "~/Documents/FDAlibrary/saved_data_structures/"
+
 # Read in data from External query
 
-human_table <- read.table("~/Documents/FDAlibrary/saved_data_structures/signaling-for-gabi.tsv", sep="\t")
-colnames(human_table) <- c("filename", "Condition", "Donor", "Population", "Reagent", "Raw_median")
+human_table <- read.table("~/Documents/FDAlibrary/saved_data_structures/signaling-for-gabi-v2.3.tsv", sep="\t")
+colnames(human_table) <- c("filename", "Population", "Reagent", "Raw_median")
 human_df <- data.frame(human_table)
 human_df$Raw_median <- as.numeric(as.character(human_df$Raw_median))
 
@@ -32,28 +34,27 @@ new_df <- human_df %>%
   dplyr::filter(Population != "CD4..CD8.") %>%
   dplyr::filter(Population != "HLA.DR..CD11c.") %>%
   dplyr::filter(Population != "singlets_basophils.") %>%
+  dplyr::filter(Population != "pDCs") %>%
   dplyr::mutate(Feature = paste(Population, Reagent, sep = "_"))
 
 new_df$Feature <- as.factor(new_df$Feature)
 feature_matrix <- dplyr::select(new_df, Population, Reagent, Feature)
 
-new_df <- dplyr::select(new_df, filename, Donor, Condition, Feature, Raw_median)
+new_df <- dplyr::select(new_df, filename, Feature, Raw_median)
 
 
 # Change to wide format
 
-wide_df <- dcast(new_df, filename + Donor + Condition ~ Feature, value.var = "Raw_median")
+wide_df <- dcast(new_df, filename ~ Feature, value.var = "Raw_median")
 
 
 # Get the annotations from the server
-baseUrl <- 54.186.1.226
 access_key <- authenticate("gfragiadakis", "password", baseURL =  "http://54.186.1.226/api/v1")
 
 experiments <- get_experiments(access_key)
 
 experimentID <- experiments[experiments$name == "All Species", '_id']
 FCS_files <- get_FCS_files(experimentID, access_key)
-FCS_files <- FCS_files[1:2624,] # should fix annotations function so don't have to do it
 annotations <- get_annotations(FCS_files)
 human_annotations <- dplyr::filter(annotations,Species == "Human")
 
@@ -67,15 +68,13 @@ levels(annotated_df$Donor) <- c(levels(annotated_df$Donor), "7605_2", "7962_2")
 annotated_df[grep("R20W3", annotated_df$filename), "Donor"] <- "7605_2"
 annotated_df[grep("R25W5", annotated_df$filename), "Donor"] <- "7962_2"
 
-
-
-write.csv(annotated_df, file = "~/Documents/FDAlibrary/saved_data_structures/human_signaling_raw.csv")
+write.csv(annotated_df, file = paste(dataDirectory, "human_signaling_raw.csv", sep = ""))
 
 fold_df <- get_folds(annotated_df, basal_name = "Basal", fold_type = "raw")
 asinh_df <- get_folds(annotated_df, basal_name = "Basal", fold_type = "asinh")
 
-write.csv(fold_df, file = "~/Documents/FDAlibrary/saved_data_structures/human_signaling_fold.csv")
-write.csv(asinh_df, file = "~/Documents/FDAlibrary/saved_data_structures/human_signaling_fold_asinh.csv")
+write.csv(fold_df, file = paste(dataDirectory, "human_signaling_fold.csv", sep = ""))
+write.csv(asinh_df,file = paste(dataDirectory, "human_signaling_fold_asinh.csv", sep = ""))
 
-
-
+compiled_df <- threshold_features(data = asinh_df, threshold = 0.2)
+write.csv(compiled_df, file = paste(dataDirectory, "human_signaling_fold_asinh_0.2.csv", sep = ""))
--- analyses/plot-scripts.R
+++ analyses/plot-scripts.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
 
+plotDirectory <- "~/Documents/FDAlibrary/analyses/plots/"
+
 for (i in levels(asinh_df$Condition)){
   if (i != "Basal"){
     # make this a function (get long format for a given condition)
@@ -10,7 +12,7 @@ for (i in levels(asinh_df$Condition)){
 
     p <- ggplot(df, aes(Feature, Asinh_ratio))
     p + geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle(i)
-    ggsave(paste(i,"_feature_boxplots.pdf", sep = ""), width = 40, height = 10)
+    ggsave(paste(plotDirectory, i,"_feature_boxplots.pdf", sep = ""), width = 40, height = 10)
   }
 
 }
@@ -18,36 +20,83 @@ for (i in levels(asinh_df$Condition)){
 
 compiled_df <- threshold_features(data = asinh_df, threshold = 0.2)
 
+plotDirectory <- "~/Documents/FDAlibrary/analyses/plots/thresholded_asinh_0.2/"
+
 f_df <- dplyr::filter(compiled_df, Gender == "F")
 m_df <- dplyr::filter(compiled_df, Gender == "M")
 
-f_grouped <- group_by(f_df, Feature_Condition)
+f_grouped <- group_by(f_df, Condition_Feature)
 f_sum <- dplyr::summarise(f_grouped, f_mean = mean(value), f_sd = sd(value))
 
-m_grouped <- group_by(m_df, Feature_Condition)
+m_grouped <- group_by(m_df, Condition_Feature)
 m_sum <- dplyr::summarise(m_grouped, m_mean = mean(value), m_sd = sd(value))
 
 df_gender <- data.frame(m_sum, f_sum)
-ids <- strsplit(as.character(df_gender$Feature_Condition), "_")
+ids <- strsplit(as.character(df_gender$Condition_Feature), "_")
 df_gender <- data.frame(df_gender, Condition = sapply(ids, "[[", 1),
                         Cell_type = sapply(ids, "[[", 2),
                         Protein = sapply(ids, "[[", 3))
 
 
 p <- ggplot(df_gender, aes(m_mean, f_mean))
-p + geom_point() + geom_text(x = 0.25, y = 2, label = "R = 0.9879372") + geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)
-ggsave("men_v_women_mean.pdf")
+p + geom_point() + geom_text(x = 0.25, y = 2, label = paste("R = ", round(cor(df_gender$m_mean, df_gender$f_mean),digits = 3), sep = "")) + geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)
+ggsave(paste(plotDirectory, "men_v_women_mean.pdf", sep = ""))
 
 p <- ggplot(df_gender, aes(m_sd, f_sd))
-p + geom_point() + geom_text(x = 0.25, y = 0.75, label = "R = 0.9054926") + geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)
-ggsave("men_v_women_sd.pdf")
+p + geom_point() + geom_text(x = 0.25, y = 0.75, label = paste("R = ", round(cor(df_gender$m_sd, df_gender$f_sd),digits = 3), sep = "")) + geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)
+ggsave(paste(plotDirectory, "men_v_women_sd.pdf", sep = ""))
 
-p + geom_point(aes(colour = Condition)) + geom_text(x = 0.25, y = 0.75, label = "R = 0.9054926") + geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)
-ggsave("men_v_women_sd_by_condition.pdf")
+p + geom_point(aes(colour = Condition)) + geom_text(x = 0.25, y = 0.75, label = paste("R = ", round(cor(df_gender$m_sd, df_gender$f_sd),digits = 3), sep = "")) + geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x)
+ggsave(paste(plotDirectory, "men_v_women_sd_by_condition.pdf", sep = ""))
 
-p <- ggplot(compiled_df, aes(Feature_Condition, value))
+p <- ggplot(compiled_df, aes(Condition_Feature, value))
 p + geom_boxplot(aes(colour = Condition)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Thresholded features")
-ggsave("thresholded_asinh_0.2/thresholded_feature_boxplots_by_Condition.pdf", width = 40, height = 10)
+ggsave(paste(plotDirectory, "thresholded_feature_boxplots_by_Condition.pdf", sep = ""), width = 40, height = 10)
+
+p <- ggplot(compiled_df, aes(Condition_Feature, value))
+p + geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Thresholded features")
+ggsave(paste(plotDirectory, "thresholded_feature_boxplots.pdf", sep = ""), width = 40, height = 10)
+
+
+##### Correlation #####
+df <- compiled_df %>%
+      dplyr::select(Donor, Condition_Feature, value) %>%
+      reshape2::dcast(., Donor ~ Condition_Feature) %>%
+      dplyr::select(-Donor)
+
+plot_correlations(df, cor_threshold = 0.5, output_directory = paste(plotDirectory, "correlation/", sep = ""), background = "black")
+
+plot_correlations(df, cor_threshold = 0.5, output_directory = paste(plotDirectory, "correlation/", sep = ""), background = "white")
+
+
+### Looking at stims ###
+df <- compiled_df %>%
+  dplyr::select(Donor, Condition_Feature, value) %>%
+  group_by(Condition_Feature) %>%
+  dplyr::summarise(mean = mean(value))
+
+feats <- compiled_df %>%
+    dplyr::select(Condition, Feature, Condition_Feature) %>%
+    .[!duplicated(.),]
+
+new_df <- df %>%
+  merge(., feats) %>%
+  dplyr::select(Condition, Feature, mean) %>%
+  dcast(., Condition ~ Feature)
+
+
+new_df[is.na(new_df)] <- 0
+dat <- as.matrix(dplyr::select(new_df, -Condition))
+rownames(dat) <- new_df$Condition
+
+clustered <- cluster_matrix(dat)
+plot(clustered$rc)
+plot(clustered$cc)
+ordered_data <- clustered$data
+
+qplot(x=Var2, y=Var1, data=melted, fill=value, geom="tile") +
+  scale_fill_gradient2(low = "yellow", high = "purple", mid = "white", midpoint = 0, limits=c(-1, 3)) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(paste(plotDirectory, "heat_map3.pdf", sep = ""), width = 40, height = 20)
 
 
 
