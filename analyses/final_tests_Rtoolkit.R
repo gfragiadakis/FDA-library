4ab046f626a73ae34dd8fc139c0d61b1a385f62d
gfragiadakis <gabi.fragiadakis@gmail.com>
2015-12-14 10:28:11 -0800

    Refining testing for bulk stats

--- analyses/final_tests_Rtoolkit.R
+++ analyses/final_tests_Rtoolkit.R
@@ -1,6 +1,6 @@
 # Final tests for R toolkit for blackbuck
 
-access_key <- authenticate("gfragiadakis", "password", baseURL =  "http://54.186.1.226/api/v1")
+access_key <- authenticate("gfragiadakis", "password", baseURL =  "http://52.33.207.241/api/v1")
 
 experiments <- get_experiments(access_key)
 
@@ -31,11 +31,6 @@ annotated_FCS_list <- get_annotations(FCS_file_list)
 
 display_parameters(FCS_file_list, experimentID, access_key)
 
-# bulk statistics
-GET /experiments/:experimentId/statistics
-GET /experiments/:experimentID/bulkstatistics?fcsFileIds=["<fcsFileId1>","<fcsFileId2>"]&channels=["FSC-W","PE-A"]&statistics=["mean","median","quantile"]&populationIds=["<populationId1>","<populationId2>"]&k=0.95
-
-
 # Once have bulk statistics,
 # test folds
 # test thresholds
@@ -45,3 +40,32 @@ population_names <- c("pDCs","CD14+ Monocytes")
 test <- get_statistics_set_bulk(experimentID, FCS_files, access_key, channel_names = channel_names,
   statistic_types = c("median"), q=NULL, population_names = population_names, query_limit = 900)
 
+### Testing bulk statistics: Run this for testing
+
+access_key <- authenticate("gfragiadakis", "Anatoli4%Riza", baseURL =  "http://52.33.207.241/api/v1")
+
+experiments <- get_experiments(access_key)
+
+experimentID <- experiments[experiments$name == "All Species", '_id']
+
+experiment <- get_experiment(experimentID, access_key)
+
+FCS_file_list <- get_FCS_files(experimentID, access_key)
+
+annotations <- get_annotations(FCS_file_list)
+
+FCS_files <- FCS_file_list[annotations$Species == "Human",]
+
+disp <- display_parameters(FCS_files, experimentID, access_key)
+
+# 10 populations
+population_names <- disp$populations[c(2, 6, 9, 10, 14, 16, 18, 19, 20, 21)]
+
+# 16 reagents
+reagent_names <- disp$reagents[c(12, 17, 18, 19, 20, 22, 23, 25, 27, 33, 34, 36, 38, 42, 43, 45)]
+
+# 1387 FCS_files; 221,920 requests
+query_num <- 10*16*1387
+
+statistics_frame <- get_statistics_set_bulk(experimentID, FCS_files, access_key, reagent_names = reagent_names,
+                      statistic_types = c("median"), q=NULL, population_names = population_names, query_limit = 10000)
